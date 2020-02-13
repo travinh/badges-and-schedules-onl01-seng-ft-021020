8016020eac77b017177a3195c9fcfabc267e9958
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-13 00:59:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map do |attendee|
+    "Hello, my name is #{attendee}."
+  end
+end
+
+def assign_rooms(speakers)
+  room=0
+  
+  speakers.map do |speaker|
+    room+=1
+    "Hello, #{speaker}! You'll be assigned to room #{room}!"
+  end
+end
+
+def printer(attendees)
+  array2 = batch_badge_creator(attendees)
+  array2.each do |item|
+    puts item
+  end
+  array3 = assign_rooms(attendees)
+  
+end
